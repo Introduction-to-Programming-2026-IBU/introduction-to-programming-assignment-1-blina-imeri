51a746093131ac990f1d2805525878fc3bff2884
Blina Imeri <blina.imeri10@ibu.edu.mk>
2026-03-01 11:51:07 +0100

    Required Assignments of week one

---
+++ Mario.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+
+    do
+    {
+        printf("How high do you want the pyramid to be? -");
+        scanf("%i", &height);
+
+    }
+    while (height<0 || height>9);
+
+    for (int row = 1; row <= height; row++)
+    {
+        for(int spaces = 0; spaces < height - row; spaces++)
+        {
+            printf(" "); // Two spaces for alignment
+        }
+        for(int i=0; i<row; i++)
+	    {
+		printf("#");
+	    }        
+        printf("\n");
+    }
+
+    return 0;
+}
