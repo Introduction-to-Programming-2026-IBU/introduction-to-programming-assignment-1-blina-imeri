c742a9da49231b3434603511b80fa5e8e643d868
Blina Imeri <blina.imeri10@ibu.edu.mk>
2026-04-18 09:01:36 +0200

    done2

--- week7/challenges/challenge2.py
+++ week7/challenges/challenge2.py
@@ -4,3 +4,23 @@
 import csv
 
 # Your code here
+with open('favorites.csv') as f:
+    reader = csv.reader(f)
+    next(reader)  # Skip header row
+
+    language_problems = {}
+
+    for row in reader:
+        _, language, problem = row
+        if language not in language_problems:
+            language_problems[language] = {}
+        if problem not in language_problems[language]:
+            language_problems[language][problem] = 0
+        language_problems[language][problem] += 1
+
+    print(f"{'Language':<20} {'Most Common Problem':<20}")
+    print("-" * 40)
+
+    for language, problems in language_problems.items():
+        most_common_problem = max(problems, key=problems.get)
+        print(f"{language:<20} {most_common_problem:<20}")
